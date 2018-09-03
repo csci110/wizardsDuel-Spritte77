785c2ad6adfc1ce645267cca3a00e9509ce9f234
Megan Huffnagel <huffnagelm@dewv.edu>
2018-09-03 16:53:48 +0000

    Updates via sync script

--- wizardsDuel.js
+++ wizardsDuel.js
@@ -1,26 +1,24 @@
-import {game, Sprite} from "./sgc/sgc.js";
+import { game, Sprite } from "./sgc/sgc.js";
 game.setBackground("floor.png");
 
-class PlayerWizard extends Sprite { 
-    Constructor() {
-defineAnimation(Marcus, 0, 12);
-this.definieAnimation("down", 6, 8);
-this.speedWhenWalking = 100;
+class PlayerWizard extends Sprite {
+    constructor() {
+        super();
+        this.name = "Marcus the Wizard";
+        this.setImage("marcusSheet.png");
+        this.defineAnimation("down", 6, 8);
+        this.defineAnimation("up", 0, 2);
+        this.width = 48;
+        this.height = 48;
+        this.x = this.width;
+        this.y = this.height;
+        this.speedWhenWalking = 100;
     }
     handleDownArrowKey() {
-    this.playAnimation("down");
-    this.speed = this.whenSpeedWalking;
-    this.angle = 270;
+        this.playAnimation("down");
+        this.speed = this.speedWhenWalking;
+        this.angle = 270;
+    }
 }
-}
 
-
-
-let Marcus = new PlayerWizard ();
-Marcus.name = "Marcus the Wizard";
-Marcus.setImage("marcusSheet.png");
-Marcus.width = 48;
-Marcus.height = 48;
-Marcus.x = this.width;
-Marcus.y = this.y;
-
+let Marcus = new PlayerWizard();
