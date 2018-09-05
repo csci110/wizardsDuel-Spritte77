9f9ab7a5059d61e7cca6500c05cbb50222abf06d
Megan Huffnagel <huffnagelm@dewv.edu>
2018-09-05 17:03:35 +0000

    Updates via sync script

--- wizardsDuel.js
+++ wizardsDuel.js
@@ -19,6 +19,34 @@ class PlayerWizard extends Sprite {
         this.speed = this.speedWhenWalking;
         this.angle = 270;
     }
+    handleUpArrowKey() {
+        this.playAnimation("up");
+        this.speed = this.speedWhenWalking;
+        this.angle = 90;
+    }
+    handleGameLoop() { //Keep marcus in display area
+        this.y = Math.max(0, this.y);
+        this.y = Math.min(game.displayHeight - this.height, this.y);
+        this.speed = 0;
+    }
+    handleSpacebar() {
+        let spell = new Spell();
+        spell.name = "A spell cast by Marcus";
+        spell.setImage("marcusSpellSheet.png");
+        spell.x = this.x; //this sets the position of the spell object to equal to
+        spell.y = this.y; //the position of any object in the player wizard class
+        spell.angle = 0;
+    }
 }
+    let Marcus = new PlayerWizard();
 
-let Marcus = new PlayerWizard();
+class Spell extends Sprite {
+    constructor(){
+        super();
+        this.speed = 200;
+        this.height = 48;
+        this.width = 48;
+        this.defineAnimation("magic", 0, 7);
+        this.playAnimation("magic", true);
+    }
+}
